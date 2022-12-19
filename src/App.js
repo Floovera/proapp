b4cd11f6ec3ee3aa46c5d63adeca82cda88cf724
Floovera <r0385588@student.thomasmore.be>
2022-12-19 14:25:23 +0100

    added existing app to proapp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import 'normalize.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {TOPIC_DATA} from "./data/data";
+import {Tab, TabList, TabPanel, Tabs} from "react-tabs";
+import {TopicsPage} from "./pages/TopicsPage";
+import 'react-tabs/style/react-tabs.css';
+import 'react-bootstrap/dist/react-bootstrap.min'
+import "./services/firebase";
+import {StudentsFromDbPage} from "./pages/StudentsFromDb";
+import {FormPage} from "./pages/FormPage";
+import {useLocalStorage} from "./hooks/useLocalStorage";
+import {StudentsFromDbProvider} from "./contexts/StudentContext";
+import {TopicsFromDbPage} from "./pages/TopicsFromDb";
+import {TopicsFromDbProvider} from "./contexts/NewTopicContext";
+import {OverviewPage} from "./pages/OverviewPage";
 
-function App() {
+function ProvidedApp() {
+
+    const [defaultTab,setDefaultTab] = useLocalStorage("defaultTabIndex",0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and saveeeee to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Tabs defaultIndex={defaultTab} onSelect={(index) => setDefaultTab(index)}>
+
+          <TabList>
+              <Tab>To do list (d&d)</Tab>
+              <Tab>Topics (d&d)</Tab>
+              <Tab>Students</Tab>
+              <Tab>Topics</Tab>
+              <Tab>Overview</Tab>
+          </TabList>
+          <TabPanel>
+              <FormPage/>
+          </TabPanel>
+          <TabPanel>
+              <TopicsPage topiclist={TOPIC_DATA}/>
+          </TabPanel>
+          <TabPanel>
+              <StudentsFromDbPage/>
+          </TabPanel>
+          <TabPanel>
+              <TopicsFromDbPage/>
+          </TabPanel>
+          <TabPanel>
+              <OverviewPage/>
+          </TabPanel>
+      </Tabs>
   );
 }
 
+function App() {
+    return (
+        <TopicsFromDbProvider>
+            <StudentsFromDbProvider>
+                <ProvidedApp/>
+            </StudentsFromDbProvider>
+        </TopicsFromDbProvider>
+
+    );
+}
+
 export default App;
+
